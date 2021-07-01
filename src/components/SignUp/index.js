5e1792243b585f4c84c5b50654f8d1ffb74f3faf
Adeoti Adegboyega <adeotiadegboyega@gmail.com>
2021-07-01 06:48:21 +0100

    created the form inputs in the signup page and styled it

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,21 +1,75 @@
-import React, {Component} from 'react';
-import './styles.css';
+import React, { Component } from "react";
+import FormInput from "../forms/FormInput";
+import Button from "../forms/Button";
+import "./styles.css";
 
+const initialState = {
+	displayName: "",
+	email: "",
+	password: "",
+	confirmPassword: "",
+};
 
 class SignUp extends Component {
-    render() {
-    return ( 
-        <div className="sign-up">
-            <div className="sign-up__wrapper">
-                <h2>SignUp</h2>
-                <form action="">
+	constructor(props) {
+		super(props);
+		this.state = {
+			...initialState,
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-                </form>
-            </div>
+	handleChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value,
+		});
+	};
+	render() {
+		const { displayName, email, password, confirmPassword } = this.state;
+		console.log(displayName);
 
-        </div>
-     );
-    }
+		return (
+			<div className="sign-up">
+				<div className="sign-up__wrapper">
+					<h2>SignUp</h2>
+					<form action="">
+						<FormInput
+							type="text"
+							name="displayName"
+							value={displayName}
+							placeholder="Full name"
+							onChange={this.handleChange}
+						/>
+						<FormInput
+							type="text"
+							name="email"
+							value={email}
+							placeholder="Email"
+							onChange={this.handleChange}
+						/>
+						<FormInput
+							type="password"
+							name="password"
+							value={password}
+							placeholder="Password"
+							onChange={this.handleChange}
+						/>
+						<FormInput
+							type="password"
+							name="confirmPassword"
+							value={confirmPassword}
+							placeholder="Confirm Password"
+							onChange={this.handleChange}
+						/>
+                        <Button type="submit">
+                            Register
+                        </Button>
+					</form>
+				</div>
+			</div>
+		);
+	}
 }
- 
+
 export default SignUp;
--- src/components/forms/FormInput/index.js
+++ src/components/forms/FormInput/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './styles.css'
 
 const FormInput = ({handleChange, label, ...otherProps}) => {
     return ( 
         <div className="form-group__row">
+            {label && (
+                <label>
+                    {label}
+                </label>
+            )}
 
             <input className="form-input"  onChange={handleChange} {...otherProps}/>
         </div>
